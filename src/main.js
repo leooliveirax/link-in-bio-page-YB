cbf47f50345ec0bc3362f5d2c288dd8b0e6c2a24
leonardoXoliveira <xcontato.leonardo.x.oliveirax@gmail.com>
2022-09-17 15:49:41 -0300

    feat: finish first release 🚀

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js";
-import "https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.halo.min.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js";
+import "https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  VANTA.HALO({
+  VANTA.NET({
     el: "#vantaBackground",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
     minHeight: 200.0,
     minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
   });
 
   setTimeout(() => {
